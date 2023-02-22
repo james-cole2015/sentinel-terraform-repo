0334ade0d34bad389c496444db6d7f54ba0a52e1
brooks <brooks.houck@gmail.com>
2023-02-22 20:50:50 +0000

    updating tags

--- main.tf
+++ main.tf
@@ -1,41 +1,41 @@
 resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/24"
-    tags {
+    tags = {
         Name = VPC00
     }
 }
 
 resource "aws_vpc" "main1" {
     cidr_block = "11.0.0.0/24"
-    tags {
+    tags = {
         Name = VPC01
     }
 }
 
 resource "aws_vpc" "main2" {
     cidr_block = "12.0.0.0/24"
-    tags {
+    tags = {
         Name = VPC02
     }
 }
 
 resource "aws_vpc" "main3" {
     cidr_block = "13.0.0.0/24"
-    tags {
+    tags = {
         Name = VPC03
     }
 }
 
 resource "aws_vpc" "main4" {
     cidr_block = "14.0.0.0/24"
-    tags {
+    tags = {
         Name = VPC04
     }
 }
 
 resource "aws_vpc" "main5" {
     cidr_block = "15.0.0.0/24"
-    tags {
+    tags = 
         Name = VPC05
     }
 }
